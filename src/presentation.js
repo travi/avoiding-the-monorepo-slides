994ecba5fe8ad3ebdfb6458366aad3793a21838b
Matt Travi <programmer@travi.org>
2018-11-04 10:54:25 -0600

    feat(latest): pulled in the latest changes

--- src/presentation.js
+++ src/presentation.js
@@ -23,46 +23,104 @@ export default function Presentation() {
         <Heading size={1} fit caps lineHeight={1}>
           Avoiding the Monorepo
         </Heading>
-        <Text textSize="1em" margin="40px 0px 0px" bold>Matt Travi</Text>
+        <Text textSize="1em" margin="40px 0px 0px" bold>
+          Matt Travi
+        </Text>
       </Slide>
       <Slide bgColor="black" transition={['slide']}>
-        <Heading size={1} fit>Monolith vs Modularity</Heading>
+        <Heading size={1} fit>
+          Monolith vs Modularity
+        </Heading>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Splitting the Monolith</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Splitting the Monolith
+        </Heading>
         <List>
-          <Appear><ListItem>Important to understand trade-offs</ListItem></Appear>
-          <Appear><ListItem>Undeniable benefits</ListItem></Appear>
-          <Appear><ListItem>Account for costs when splitting</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              Important to understand trade-offs
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Undeniable benefits
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Account for costs when splitting
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Costs of Modularity</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Costs of Modularity
+        </Heading>
         <List>
-          <Appear><ListItem>Unpublished changes</ListItem></Appear>
-          <Appear><ListItem>Published versions not yet consumed</ListItem></Appear>
-          <Appear><ListItem>Duplication of decisions/implementations</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              Unpublished changes
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Published versions not yet consumed
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Duplication of decisions/implementations
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Management Options</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Management Options
+        </Heading>
         <List>
-          <Appear><ListItem>node.js/npm are well suited for extreme modularity</ListItem></Appear>
-          <Appear><ListItem>Repositories 1:1 for npm packages</ListItem></Appear>
-          <Appear><ListItem>Monorepos that version many npm packages</ListItem></Appear>
-          <Appear><ListItem>I consider monorepos to be a symptom</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              node.js/npm are well suited for extreme modularity
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Repositories 1:1 for npm packages
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Monorepos that version many npm packages
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              I consider monorepos to be a symptom
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide bgColor="black" transition={['slide']}>
-        <Heading size={1} fit>Continuous Deployment</Heading>
-        <Heading size={2} textColor="primary" fit>Automate Publishing and Updating</Heading>
+        <Heading size={1} fit>
+          Continuous Deployment
+        </Heading>
+        <Heading size={2} textColor="primary" fit>
+          Automate Publishing and Updating
+        </Heading>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Semantic Release</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Semantic Release
+        </Heading>
         <Image src={images.semanticReleaseLogo.replace('/', '')} width="50%" />
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="#00c775" textFont="primary" caps fit>Greenkeeper</Heading>
+        <Heading size={3} textColor="#00c775" textFont="primary" caps fit>
+          Greenkeeper
+        </Heading>
         <Image
           src={require('../assets/greenkeeper.svg')}
           width="50%"
@@ -74,25 +132,39 @@ export default function Presentation() {
       {/* <Image src={images.pr.replace('/', '')} /> */}
       {/* </Slide> */}
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Greenkeeper Keeper</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Greenkeeper Keeper
+        </Heading>
         <Image src={images.greenkeeperKeeperLogo.replace('/', '')} width="50%" />
       </Slide>
       <Slide bgColor="black" transition={['slide']}>
-        <Heading size={1} fit>Project Scaffolding</Heading>
-        <Heading size={2} textColor="primary" fit>Codify Your Decisions</Heading>
+        <Heading size={1} fit>
+          Project Scaffolding
+        </Heading>
+        <Heading size={2} textColor="primary" fit>
+          Codify Your Decisions
+        </Heading>
       </Slide>
       <Slide bgColor="white">
         <Typeface googleFont="Roboto Slab">
-          <Heading size={3} textColor="black" textFont="primary" caps fit>Yeoman</Heading>
+          <Heading size={3} textColor="black" textFont="primary" caps fit>
+            Yeoman
+          </Heading>
         </Typeface>
         <Image src={images.yeoman.replace('/', '')} width="50%" />
       </Slide>
       <Slide bgColor="white">
         <Typeface googleFont="Roboto Slab">
-          <Heading size={3} textColor="black" textFont="primary" caps fit>Yeoman</Heading>
+          <Heading size={3} textColor="black" textFont="primary" caps fit>
+            Yeoman
+          </Heading>
         </Typeface>
         <List>
-          <Appear><ListItem>De facto standard generator for JavaScript projects</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              De facto standard generator for JavaScript projects
+            </ListItem>
+          </Appear>
           <Appear>
             <ListItem>
               <Link textColor="black" href="http://yeoman.io/generators/">
@@ -100,72 +172,136 @@ export default function Presentation() {
               </Link>
             </ListItem>
           </Appear>
-          <Appear><ListItem>Ended up not fitting how I wanted to compose generators</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              Ended up not fitting how I wanted to compose generators
+            </ListItem>
+          </Appear>
           <Appear>
             <ListItem>
               So, I&apos;m now building&nbsp;
-              <Link textColor="black" href="https://github.com/travi/project-scaffolder">my own scaffolding tool</Link>
+              <Link textColor="black" href="https://github.com/travi/project-scaffolder">
+                my own scaffolding tool
+              </Link>
             </ListItem>
           </Appear>
         </List>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Foundational Decisions</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Foundational Decisions
+        </Heading>
         <List>
-          <Appear><ListItem>git configuration</ListItem></Appear>
-          <Appear><ListItem>Public/Private</ListItem></Appear>
-          <Appear><ListItem>License</ListItem></Appear>
-          <Appear><ListItem>Build (CI) service</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              git configuration
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Public/Private
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              License
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Build (CI) service
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Language Decisions</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Language Decisions
+        </Heading>
         <List>
-          <Appear><ListItem>Deployment Strategy</ListItem></Appear>
-          <Appear><ListItem>Testing Strategy</ListItem></Appear>
-          <Appear><ListItem>Build Automation</ListItem></Appear>
-          <Appear><ListItem>Commit Conventions</ListItem></Appear>
-          <Appear><ListItem>Version</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              Deployment Strategy
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Testing Strategy
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Build Automation
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Commit Conventions
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Version
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide bgColor="black" transition={['slide']}>
-        <Heading size={1} fit>Scaffolder Demo</Heading>
+        <Heading size={1} fit>
+          Scaffolder Demo
+        </Heading>
       </Slide>
       <Slide bgColor="white">
-        <Heading size={3} textColor="black" textFont="primary" caps fit>Summary</Heading>
+        <Heading size={3} textColor="black" textFont="primary" caps fit>
+          Summary
+        </Heading>
         <List>
-          <Appear><ListItem>Automate Publishing</ListItem></Appear>
-          <Appear><ListItem>Isolate Updates</ListItem></Appear>
-          <Appear><ListItem>Automate Updates (if you have enough confidence)</ListItem></Appear>
-          <Appear><ListItem>Codify Decisions</ListItem></Appear>
+          <Appear>
+            <ListItem>
+              Automate Publishing
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Isolate Updates
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Automate Updates (if you have enough confidence)
+            </ListItem>
+          </Appear>
+          <Appear>
+            <ListItem>
+              Codify Decisions
+            </ListItem>
+          </Appear>
         </List>
       </Slide>
       <Slide transition={['zoom']}>
-        <Heading size={1} fit>Matt Travi</Heading>
+        <Heading size={1} fit>
+          Matt Travi
+        </Heading>
 
         <List>
           <ListItem>
-            <Link textColor="white" target="_blank" href="https://matt.travi.org">matt.travi.org</Link>
+            <Link textColor="white" target="_blank" href="https://matt.travi.org">
+              matt.travi.org
+            </Link>
           </ListItem>
           <ListItem>
-            <Link textColor="white" target="_blank" href="https://twitter.com/mtravi">twitter.com/mtravi</Link>
+            <Link textColor="white" target="_blank" href="https://twitter.com/mtravi">
+              twitter.com/mtravi
+            </Link>
           </ListItem>
           <ListItem>
-            <Link
-              textColor="white"
-              target="_blank"
-              href="https://github.com/travi/project-scaffolder/tree/dsmjs-may-2018"
-            >
+            <Link textColor="white" target="_blank" href="https://github.com/travi/project-scaffolder">
               github.com/travi/project-scaffolder
             </Link>
           </ListItem>
           <ListItem>
-            <Link
-              textColor="white"
-              target="_blank"
-              href="https://presentations.travi.org/avoid-the-monolith-icc-may-2018"
-            >
-              presentations.travi.org/avoid-the-monolith-icc-may-2018
+            <Link textColor="white" target="_blank" href="https://presentations.travi.org/avoiding-the-monorepo">
+              presentations.travi.org/avoiding-the-monorepo
             </Link>
           </ListItem>
         </List>
